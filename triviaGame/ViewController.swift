bf4417d29684d693a41ff430b91e265c232c80b5
Spencer Casteel <spencercasteel@gmail.com>
2018-10-08 12:32:16 -0500

    adding initial files

---
+++ triviaGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  triviaGame
+//
+//  Created by Spencer Casteel on 10/8/18.
+//  Copyright © 2018 Spencer Casteel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

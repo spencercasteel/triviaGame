f4fd8d4db8953c3215ae01ff7b86a64c7eb38a21
Spencer Casteel <spencercasteel@gmail.com>
2018-10-09 14:49:26 -0500

    added question 1

--- triviaGame/ViewController.swift
+++ triviaGame/ViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answer1Button: UIButton!
+    @IBOutlet weak var answer2Button: UIButton!
+    @IBOutlet weak var answer3Button: UIButton!
+    @IBOutlet weak var answer4Button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let newQuestion = TriviaQuestion(question: "is jake gay?", answers: ["yes", "no", "maybe", "just on the days that end with y"], correctAnswerIndex: 3)
+        questionLabel.text = newQuestion.question
+        answer1Button.setTitle(newQuestion.answers[0], for: .normal)
+        answer2Button.setTitle(newQuestion.answers[1], for: .normal)
+        answer3Button.setTitle(newQuestion.answers[2], for: .normal)
+        answer4Button.setTitle(newQuestion.answers[3], for: .normal)
+        
     }
 
-
+    
+    @IBAction func answerButtonTapped(_ sender: UIButton) {
+        if sender.tag == 3 {
+            let newAlert = UIAlertController(title: "Thats right", message: "jake is gay everyday", preferredStyle: .actionSheet)
+            let closeAction = UIAlertAction(title: "close", style: .default, handler: nil)
+            newAlert.addAction(closeAction)
+            self.present(newAlert, animated: true, completion: nil)
+            
+        } else {
+            let newAlert = UIAlertController(title: "thats wrong", message: "better luck next time", preferredStyle: .actionSheet)
+            let closeAction = UIAlertAction(title: "close", style: .default, handler: nil)
+            newAlert.addAction(closeAction)
+            self.present(newAlert, animated: true, completion: nil)
+        }
+    }
 }
+
 
